a46c991c736447ac0c12518fafb5bf2b16dc4a3d
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-26 15:08:47 +0530

    added initial code

--- confluent-env/main.tf
+++ confluent-env/main.tf
@@ -12,19 +12,19 @@ provider "confluent" {
   cloud_api_secret = var.confluent_cloud_api_secret
 }
 
-# Create Confluent Environment
+# Confluent Environment
 resource "confluent_environment" "this" {
   display_name = var.environment_name
 }
 
-# Create Service Accounts
+# Service Accounts
 resource "confluent_service_account" "accounts" {
   for_each     = { for sa in var.service_accounts : sa.name => sa }
   display_name = each.value.name
   description  = "Service account for ${each.value.name}"
 }
 
-# Assign Roles
+# Role Bindings
 resource "confluent_role_binding" "bindings" {
   for_each = {
     for sa in var.service_accounts : sa.name => {
--- confluent-env/variables.tf
+++ confluent-env/variables.tf
@@ -1,15 +1,3 @@
-variable "confluent_cloud_api_key" {
-  description = "Confluent Cloud API Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "confluent_cloud_api_secret" {
-  description = "Confluent Cloud API Secret"
-  type        = string
-  sensitive   = true
-}
-
 variable "environment_name" {
   description = "Name of the Confluent environment"
   type        = string
@@ -21,4 +9,16 @@ variable "service_accounts" {
     name  = string
     roles = list(string)
   }))
+}
+
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
 }
