45ea47f6065a7c46b066635e073484dd49cda033
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-26 15:46:14 +0530

    added initial code

--- confluent-env/main.tf
+++ confluent-env/main.tf
@@ -14,31 +14,30 @@ provider "confluent" {
 
 # Create Environment
 resource "confluent_environment" "this" {
-  display_name = "${var.by_env}-environment"
+  display_name = "${var.by_env}-${var.region}-environment"
 }
 
 # Create Service Accounts
 resource "confluent_service_account" "accounts" {
   for_each = { for sa in var.service_accounts : sa.name => sa }
 
-  display_name = each.value.name
-  description  = "Service account for ${each.value.name}"
+  display_name = "${each.value.name}-${var.by_env}-${var.region}"
+  description  = "Service account for ${each.value.name} in ${var.by_env}-${var.region}"
 }
 
 # Assign environment-level roles
 resource "confluent_role_binding" "env_roles" {
   for_each = {
     for sa in var.service_accounts : sa.name => sa
+    if length(sa.roles) > 0
   }
 
   principal   = "User:${confluent_service_account.accounts[each.key].id}"
   crn_pattern = confluent_environment.this.resource_name
-
-  # Only bind environment-safe roles
-  role_name = "MetricsViewer"
+  role_name   = "MetricsViewer" # You can expand to all roles dynamically
 }
 
-# Assign organization-level roles (e.g., AccountAdmin)
+# Org-level roles (AccountAdmin only)
 resource "confluent_role_binding" "org_account_admin" {
   for_each = {
     for sa in var.service_accounts : sa.name => sa
@@ -49,3 +48,4 @@ resource "confluent_role_binding" "org_account_admin" {
   crn_pattern = "crn://confluent.cloud/organization=${var.organization_id}"
   role_name   = "AccountAdmin"
 }
+
