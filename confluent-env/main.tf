320dcf4f28e489f6d1ca71c30f69b48c7098ab6e
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-26 16:08:28 +0530

    added corrected values

--- confluent-env/main.tf
+++ confluent-env/main.tf
@@ -17,35 +17,38 @@ resource "confluent_environment" "this" {
   display_name = "${var.by_env}-${var.region}-environment"
 }
 
-# Create Service Accounts
+# Create Service Accounts with full names (name-env-region)
 resource "confluent_service_account" "accounts" {
-  for_each = { for sa in var.service_accounts : sa.name => sa }
-
-  display_name = "${each.value.name}-${var.by_env}-${var.region}"
-  description  = "Service account for ${each.value.name} in ${var.by_env}-${var.region}"
-}
-
-# Assign environment-level roles
-resource "confluent_role_binding" "env_roles" {
   for_each = {
-    for sa in var.service_accounts : sa.name => sa
-    if length(sa.roles) > 0
+    for sa in var.service_accounts :
+    "${sa.name}-${var.by_env}-${var.region}" => {
+      name  = "${sa.name}-${var.by_env}-${var.region}"
+      roles = sa.roles
+    }
   }
 
-  principal   = "User:${confluent_service_account.accounts[each.key].id}"
-  crn_pattern = confluent_environment.this.resource_name
-  role_name   = "MetricsViewer" # You can expand to all roles dynamically
+  display_name = each.value.name
+  description  = "Service account for ${each.value.name}"
 }
 
-# Org-level roles (AccountAdmin only)
+# Environment-level role bindings
+resource "confluent_role_binding" "env_roles" {
+  for_each = confluent_service_account.accounts
+
+  principal   = "User:${each.value.id}"
+  crn_pattern = confluent_environment.this.resource_name
+  role_name   = "MetricsViewer"
+}
+
+# Organization-level AccountAdmin roles
 resource "confluent_role_binding" "org_account_admin" {
   for_each = {
-    for sa in var.service_accounts : sa.name => sa
+    for name, sa in confluent_service_account.accounts :
+    name => sa
     if contains(sa.roles, "AccountAdmin")
   }
 
-  principal   = "User:${confluent_service_account.accounts[each.key].id}"
+  principal   = "User:${each.value.id}"
   crn_pattern = "crn://confluent.cloud/organization=${var.organization_id}"
   role_name   = "AccountAdmin"
 }
-
--- confluent-env/outputs.tf
+++ confluent-env/outputs.tf
@@ -1,10 +1,13 @@
 output "environment_id" {
-  value = confluent_environment.this.id
+  description = "The ID of the created Confluent environment"
+  value       = confluent_environment.this.id
 }
 
 output "service_accounts" {
+  description = "Details of created service accounts"
   value = {
-    for name, sa in confluent_service_account.accounts : name => {
+    for name, sa in confluent_service_account.accounts :
+    name => {
       id          = sa.id
       description = sa.description
     }
