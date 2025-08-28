bb5b93c9ebc56cd42b029fabc649031e349e6aa6
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-28 20:04:35 +0530

    added corrected values

--- confluent-env/d_api_key_secret_creation_and_push_to_kv.tf
+++ confluent-env/d_api_key_secret_creation_and_push_to_kv.tf
@@ -1,8 +1,8 @@
 # Lookup existing Key Vault
-data "azurerm_key_vault" "kv" {
-  name                = var.key_vault_name
-  resource_group_name = var.key_vault_rg
-}
+# data "azurerm_key_vault" "kv" {
+#   name                = var.key_vault_name
+#   resource_group_name = var.key_vault_rg
+# }
 
 # API Keys for each Service Account
 resource "confluent_api_key" "sa_keys" {
@@ -19,18 +19,18 @@ resource "confluent_api_key" "sa_keys" {
   depends_on = [confluent_role_binding.env_roles]
 }
 
-# Store API Key in Azure Key Vault
-resource "azurerm_key_vault_secret" "sa_api_key" {
-  for_each     = confluent_api_key.sa_keys
-  name         = "${each.key}-key"
-  value        = each.value.id
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
-
-# Store API Secret in Azure Key Vault
-resource "azurerm_key_vault_secret" "sa_api_secret" {
-  for_each     = confluent_api_key.sa_keys
-  name         = "${each.key}-secret"
-  value        = each.value.secret
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
+# # Store API Key in Azure Key Vault
+# resource "azurerm_key_vault_secret" "sa_api_key" {
+#   for_each     = confluent_api_key.sa_keys
+#   name         = "${each.key}-key"
+#   value        = each.value.id
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+#
+# # Store API Secret in Azure Key Vault
+# resource "azurerm_key_vault_secret" "sa_api_secret" {
+#   for_each     = confluent_api_key.sa_keys
+#   name         = "${each.key}-secret"
+#   value        = each.value.secret
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
