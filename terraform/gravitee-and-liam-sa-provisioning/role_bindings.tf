1ec8523e9bce9b7b7db2317d038d0d52f6e95d18
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-29 17:57:19 +0530

    service account role

--- terraform/gravitee-and-liam-sa-provisioning/role_bindings.tf
+++ terraform/gravitee-and-liam-sa-provisioning/role_bindings.tf
@@ -1,23 +1,26 @@
+# LIAM owns its topic
 resource "confluent_role_binding" "liam_topic_owner" {
   count = var.sa_for_liam && var.default_topic_for_liam ? 1 : 0
 
   principal   = "User:${confluent_service_account.liam[0].id}"
   role_name   = "ResourceOwner"
-  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka=${var.confluent_kafka_cluster_id}/topic=${confluent_kafka_topic.liam_default[0].topic_name}"
+  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka-cluster=${var.confluent_kafka_cluster_id}/topic=${confluent_kafka_topic.liam_default[0].topic_name}"
 }
 
-resource "confluent_role_binding" "liam_consumer_group_owner" {
+# LIAM owns its consumer group(s)
+resource "confluent_role_binding" "liam_consumer_group_admin" {
   count = var.sa_for_liam ? 1 : 0
 
   principal   = "User:${confluent_service_account.liam[0].id}"
-  role_name   = "ResourceOwner"
-  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka=${var.confluent_kafka_cluster_id}/consumer-group=${var.liam_project_name}*"
+  role_name   = "ConsumerGroupAdmin"
+  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka-cluster=${var.confluent_kafka_cluster_id}/consumer-group=${var.liam_project_name}*"
 }
 
-resource "confluent_role_binding" "gravitee_consumer_group_owner" {
+# Gravitee owns its consumer group(s)
+resource "confluent_role_binding" "gravitee_consumer_group_admin" {
   count = var.sa_for_gravitee ? 1 : 0
 
   principal   = "User:${confluent_service_account.gravitee[0].id}"
-  role_name   = "ResourceOwner"
-  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka=${var.confluent_kafka_cluster_id}/consumer-group=*"
+  role_name   = "ConsumerGroupAdmin"
+  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka-cluster=${var.confluent_kafka_cluster_id}/consumer-group=*"
 }
