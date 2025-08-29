7778b938e216e61d59da3ff7da896989926e5ef0
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-29 18:42:39 +0530

    service account role

--- terraform/gravitee-and-liam-sa-provisioning/role_bindings.tf
+++ terraform/gravitee-and-liam-sa-provisioning/role_bindings.tf
@@ -1,12 +1,7 @@
-variable "crn_pattern" {
-  description = "Global CRN pattern for Confluent resources"
-  type        = string
-  default     = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka-cluster=${var.confluent_kafka_cluster_id}"
-}
-
 locals {
   # Split on "." and keep only the first 4 parts (up to liam)
   liam_topic_prefix = join(".", slice(split(".", confluent_kafka_topic.liam_default_topic[0].topic_name), 0, 4))
+  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka-cluster=${var.confluent_kafka_cluster_id}"
 }
 
 # LIAM owns its topic
@@ -15,7 +10,7 @@ resource "confluent_role_binding" "liam_topic_owner" {
 
   principal   = "User:${confluent_service_account.liam[0].id}"
   role_name   = "ResourceOwner"
-  crn_pattern = "${var.crn_pattern}/topic=${liam_topic_prefix}*"
+  crn_pattern = "${locals.crn_pattern}/topic=${locals.liam_topic_prefix}*"
 }
 
 # LIAM owns its consumer group(s)
@@ -24,7 +19,7 @@ resource "confluent_role_binding" "liam_consumer_group_owner" {
 
   principal   = "User:${confluent_service_account.liam[0].id}"
   role_name   = "ResourceOwner"
-  crn_pattern = "${var.crn_pattern}/consumer-group=${var.liam_project_name}*"
+  crn_pattern = "${locals.crn_pattern}/consumer-group=${var.liam_project_name}*"
 }
 
 # Gravitee owns its consumer group(s)
@@ -33,5 +28,5 @@ resource "confluent_role_binding" "gravitee_consumer_group_owner" {
 
   principal   = "User:${confluent_service_account.gravitee[0].id}"
   role_name   = "ResourceOwner"
-  crn_pattern = "${var.crn_pattern}/consumer-group=*"
+  crn_pattern = "${locals.crn_pattern}/consumer-group=*"
 }
