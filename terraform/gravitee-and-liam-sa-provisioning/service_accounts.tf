43ebb22d478715772ba2cb195a6b467b298ef318
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-29 17:06:18 +0530

    service account role

--- terraform/gravitee-and-liam-sa-provisioning/service_accounts.tf
+++ terraform/gravitee-and-liam-sa-provisioning/service_accounts.tf
@@ -1,13 +1,13 @@
-# Gravitee Service Account
-resource "confluent_service_account" "gravitee" {
-  count        = var.sa_for_gravitee ? 1 : 0
-  display_name = "ef-gravitee-${var.confluent_environment_name}-sa"
-  description  = "Service account for Gravitee in ${var.confluent_environment_name}"
-}
-
-# Liam Service Account
-resource "confluent_service_account" "liam" {
-  count        = var.sa_for_liam ? 1 : 0
-  display_name = "ef-liam-${var.confluent_environment_name}-sa"
-  description  = "Service account for LIAM in ${var.confluent_environment_name}"
-}
+# # Gravitee Service Account
+# resource "confluent_service_account" "gravitee" {
+#   count        = var.sa_for_gravitee ? 1 : 0
+#   display_name = "ef-gravitee-${var.confluent_environment_name}-sa"
+#   description  = "Service account for Gravitee in ${var.confluent_environment_name}"
+# }
+#
+# # Liam Service Account
+# resource "confluent_service_account" "liam" {
+#   count        = var.sa_for_liam ? 1 : 0
+#   display_name = "ef-liam-${var.confluent_environment_name}-sa"
+#   description  = "Service account for LIAM in ${var.confluent_environment_name}"
+# }
