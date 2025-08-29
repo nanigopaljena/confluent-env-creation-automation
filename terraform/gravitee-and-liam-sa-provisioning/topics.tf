606ef6e82416eed03827e826958e912b42f19c32
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-08-29 16:30:58 +0530

    service account role

--- terraform/gravitee-and-liam-sa-provisioning/topics.tf
+++ terraform/gravitee-and-liam-sa-provisioning/topics.tf
@@ -1,14 +1,16 @@
 resource "confluent_kafka_topic" "liam_default" {
   count = var.default_topic_for_liam ? 1 : 0
 
-  environment {
-    id = var.confluent_environment_id
-  }
-
   kafka_cluster {
     id = var.confluent_kafka_cluster_id
   }
 
   topic_name       = "${var.by_env}.${var.region}.global.liam.gravitee-audit.event"
   partitions_count = 3
+  rest_endpoint    = "https://pkc-56d1g.eastus.azure.confluent.cloud:443"
+  credentials {
+    key    = var.confluent_cloud_api_key
+    secret = var.confluent_cloud_api_secret
+  }
 }
+
