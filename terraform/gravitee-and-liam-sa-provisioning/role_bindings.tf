626086e6451272ed3d53e5da0b738b7a1f98afa8
Nanigopal Jena <nanigopal.jena@blueyonder.com>
2025-09-03 18:33:16 +0530

    service account role

--- terraform/gravitee-and-liam-sa-provisioning/role_bindings.tf
+++ terraform/gravitee-and-liam-sa-provisioning/role_bindings.tf
@@ -1,22 +1,20 @@
 locals {
   # Split on "." and keep only the first 4 parts (up to liam)
   liam_topic_prefix = join(".", slice(split(".", confluent_kafka_topic.liam_default_topic[0].topic_name), 0, 4))
-  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka-cluster=${var.confluent_kafka_cluster_id}"
+  crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}/environment=${var.confluent_environment_id}/kafka=${var.confluent_kafka_cluster_id}"
 #   crn_pattern = "crn://confluent.cloud/organization=${var.confluent_organization_id}"
 }
 
 # LIAM owns its topic
 resource "confluent_role_binding" "liam_topic_owner" {
-  count = var.sa_for_liam && var.default_topic_for_liam ? 1 : 0
+  principal   = "User:sa-6kwvmz2"
+  role_name   = "DeveloperWrite"
+  crn_pattern = "${local.crn_pattern}/topic=dev.centralus.global.liam.gravitee-audit.event"
 
-  principal   = "User:${confluent_service_account.liam[0].id}"
-  role_name   = "ResourceOwner"
-  crn_pattern = "${local.crn_pattern}/topic=${confluent_kafka_topic.liam_default_topic[0].topic_name}"
-
-  depends_on = [
-    confluent_service_account.liam,
-    confluent_kafka_topic.liam_default_topic
-  ]
+#   depends_on = [
+#     confluent_service_account.liam,
+#     confluent_kafka_topic.liam_default_topic
+#   ]
 }
 #
 # resource "confluent_role_binding" "liam_consumer_group_owner" {
--- terraform/gravitee-and-liam-sa-provisioning/topics.tf
+++ terraform/gravitee-and-liam-sa-provisioning/topics.tf
@@ -1,16 +1,16 @@
-resource "confluent_kafka_topic" "liam_default_topic" {
-  count = var.default_topic_for_liam ? 1 : 0
-
-  kafka_cluster {
-    id = var.confluent_kafka_cluster_id
-  }
-
-  topic_name       = "${var.by_env}.${var.region}.global.liam.gravitee-audit.event"
-  partitions_count = 3
-  rest_endpoint    = "https://pkc-56d1g.eastus.azure.confluent.cloud:443"
-
-  credentials {
-    key    = var.confluent_cluster_api_key
-    secret = var.confluent_cluster_api_secret
-  }
-}
+# resource "confluent_kafka_topic" "liam_default_topic" {
+#   count = var.default_topic_for_liam ? 1 : 0
+#
+#   kafka_cluster {
+#     id = var.confluent_kafka_cluster_id
+#   }
+#
+#   topic_name       = "${var.by_env}.${var.region}.global.liam.gravitee-audit.event"
+#   partitions_count = 3
+#   rest_endpoint    = "https://pkc-56d1g.eastus.azure.confluent.cloud:443"
+#
+#   credentials {
+#     key    = var.confluent_cluster_api_key
+#     secret = var.confluent_cluster_api_secret
+#   }
+# }
